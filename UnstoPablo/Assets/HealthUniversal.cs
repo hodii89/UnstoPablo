c1baec2b9e7791fd04610012083a09e1cd27d5a9
XDDDman <114019783+XDDDman@users.noreply.github.com>
2024-06-17 18:42:52 +0200

    HealthSystem

---
+++ UnstoPablo/Assets/HealthUniversal.cs
@@ -0,0 +1,39 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class HealthUniversal : MonoBehaviour
+{
+    // Zmienna przechowuj¹ca zdrowie
+    public int health = 100;
+
+    // Funkcja do dodawania zdrowia
+    public void AddHealth(int amount)
+    {
+        health += amount;
+        // Upewnij siê, ¿e zdrowie nie przekroczy maksymalnej wartoœci, np. 100
+        health = Mathf.Min(health, 100);
+        Debug.Log("Zdrowie dodane: " + amount + ". Aktualne zdrowie: " + health);
+    }
+
+    // Funkcja do odejmowania zdrowia
+    public void SubtractHealth(int amount)
+    {
+        health -= amount;
+        // Upewnij siê, ¿e zdrowie nie spadnie poni¿ej 0
+        health = Mathf.Max(health, 0);
+        Debug.Log("Zdrowie odjête: " + amount + ". Aktualne zdrowie: " + health);
+    }
+
+    public void Update()
+    {
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject); // Zniszczenie obiektu
+    }
+}
