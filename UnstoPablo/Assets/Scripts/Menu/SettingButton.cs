be651c0eb3d2c5ecaac98f8e7d98086d17971388
hodii89 <hodii.gaming@gmail.com>
2024-06-18 15:58:42 +0200

    Menu

--- UnstoPablo/Assets/Scripts/Menu/SettingButton.cs
+++ UnstoPablo/Assets/Scripts/Menu/SettingButton.cs
@@ -1,20 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingButton : MonoBehaviour
 {
     private AudioSource audioSource;
     public AudioClip ClickingSound;
+    public GameObject SetWin;
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        SetWin.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(SetWin == true)
+        {
+            if (Input.GetKeyUp(KeyCode.Escape))
+            {
+                SetWin.SetActive(false);
+            }
+        }
+       
+    }
+    public void OpenSet()
+    {
+        Debug.Log("Settings");
+        SetWin.SetActive(true);
     }
 }
